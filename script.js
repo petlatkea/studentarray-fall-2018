cf6800621d4e170c3bd569f71094ddea360bafa3
Peter Lind <petl@kea.dk>
2018-09-09 14:58:51 +0200

    names and houses

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const names = ["Harry Potter",
+            "Ron Weasley",
+            "Hermione Granger",
+            "Neville Longbottom",
+            "Luna Lovegood",
+            "Cho Chang",
+            "Justin Finch-Fletchly",
+            "Draco Malfoy"];
+
+const houses = {
+    Gryffindor: "Gryffindor",
+    Hufflepuff: "Hufflepuff",
+    Ravenclaw: "Ravenclaw",
+    Slytherin: "Slytherin"
+}
+
+const students = [];
+
+function createStudents() {
+    // ... TODO: build this function ...
+}
